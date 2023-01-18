68089c2f089c16c0043d24890f33c67a997736a4
김태훈 <103544986+Rasen1018@users.noreply.github.com>
2023-01-18 16:37:05 +0900

    cephalo raw image stitching (2500x2400)
    (2500x2400)

--- stitching.cpp
+++ stitching.cpp
@@ -17,95 +17,76 @@ inline unsigned int clip(int value, int min, int max) {
 int main(int argc, char** argv)
 {
 	FILE* fp, * cephFP;
-	ubyte16* firstimg, *outimg;
-	int width = 48, height = 2400, cnt = 1;
+	ubyte16* outimg;
+	
+	//ceph 영상 크기로 선언
+	int width = 48, height = 2400, cnt = 0;
+	int outWidth = 2500;
 	int imgSize = width * height;
-	int outImgSize = 0;
 	bool flag = true;
 
 	struct _finddata_t fd;
 	long handle;
 	int result = 1;
 
+	// 정합 결과 영상에 사용할 메모리 할당
+	outimg = (ubyte16*)malloc(sizeof(ubyte16) * outWidth * height);
+	memset(outimg, 0, sizeof(ubyte16) * outWidth * height);
+
 	// 폴더 내 raw 파일 모두 읽어오기
-	//std::string path = "./Ceph_10/";
 	std::string path = "./Ceph_48x2400/";
 
-	//handle = _findfirst("./Ceph_10/*.raw", &fd);
 	handle = _findfirst("./Ceph_48x2400/*.raw", &fd);
 	if (handle == -1) return -1;
 
 	while (result != -1)
 	{
-		// 임시 raw 파일 메모리 할당
-		outImgSize = (width + (2 * cnt)) * height;
-		outimg = (ubyte16*)malloc((sizeof(ubyte16) * outImgSize));
-
-		ubyte16* nextimg = (ubyte16*)malloc(sizeof(ubyte16) * imgSize);
+		// 정합 전 raw 파일 메모리 할당
+		ubyte16* oneimg = (ubyte16*)malloc(sizeof(ubyte16) * imgSize);
 
 		// raw 파일 경로 저장
 		std::string name = path + fd.name;
 
-		if (flag) {
-			fp = fopen(name.c_str(), "rb");
-			firstimg = (ubyte16*)malloc(sizeof(ubyte16) * imgSize);
-			fread(firstimg, sizeof(ubyte16), imgSize, fp);
-			flag = false;
+		// 파일마다 파일 버퍼에 저장
+		if ((cephFP = fopen(name.c_str(), "rb")) == NULL) {
+			printf("never give up, %d", fd.size);
+			return -1;
 		}
-		else {
+		printf("파일명 : %s\n", name.c_str());
 
-			if ((cephFP = fopen(name.c_str(), "rb")) == NULL) {
-				printf("never give up, %d", fd.size);
-				return -1;
-			}
+		// 파일을 버퍼로 읽어오기
+		memset(oneimg, 0, (sizeof(ubyte16) * imgSize));
+		fread(oneimg, sizeof(ubyte16) * imgSize, 1, cephFP);
 
-			printf("파일명 : %s, cnt : %d \n", name.c_str(), cnt);
-
-			fread(nextimg, sizeof(ubyte16), imgSize, cephFP);
-
-			memset(outimg, 0, sizeof(ubyte16) * outImgSize);
-
-			for (int y = 0; y < height; y++) {
-				for (int x = 0; x < (width + (2 * cnt)); x++) {
-					if (x < (width + (2 * cnt) - 2))
-						outimg[y * (width + (2 * cnt)) + x] = firstimg[y * width + x];
-					else
-						outimg[y * (width + (2 * cnt)) + x] = nextimg[y * width + (x-(2*cnt))];
+		// 2픽셀씩 정합
+		for (int y = 0; y < height; y++) {
+			int i = 0;
+			for (int x = 0; x < width; x++) {
+				if (width / 2 < x && x < width / 2 + 3) {
+					outimg[y * outWidth + (i + cnt)] = ~oneimg[y * width + x];
+					i++;
 				}
 			}
-
-			//free(firstimg);
-
-			//firstimg = (ubyte16*)malloc((sizeof(ubyte16) * outImgSize));
-			//memset(firstimg, 0, (sizeof(ubyte16) * outImgSize));
-			//memcpy(firstimg, outimg, sizeof(outimg));
-			memmove(firstimg, outimg, sizeof(outimg));
-			printf("pixel : first [ %d ], next [ %d ], outimg [ %d ] \n", firstimg[33], nextimg[33], outimg[outImgSize - 480]);			
-			//firstimg = (ubyte16*)malloc(sizeof(ubyte16) * outImgSize);
-			fclose(cephFP);
-			cnt++;
-
 		}
+		cnt += 2;
 
-		free(nextimg);
+		free(oneimg);
+		fclose(cephFP);
 
 		result = _findnext(handle, &fd);
 	}
 	_findclose(handle);
-
-	fclose(fp);
-
 	
-	if ((fp = fopen("./Ceph_10.raw", "wb")) == NULL) {
+	// 정합 결과 영상 저장
+	if ((fp = fopen("./Ceph_2500x2400.raw", "wb")) == NULL) {
 		fprintf(stderr, "cannot open this file");
 		return -1;
 	}
 
-	fwrite(outimg, sizeof(ubyte16), outImgSize, fp);
+	fwrite(outimg, sizeof(ubyte16),  outWidth * height, fp);
 
 	fclose(fp);
 	free(outimg);
-	free(firstimg);
 
 	return 0;
 }
